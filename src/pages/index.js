d23e9f6862054acfa472848c2be184921d7eda05
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-07-30 16:43:26 +0700

    [FEAT]: added logout button in header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 export { default as Home } from "./Home";
 export { default as Tasks } from "./Tasks";
 export { default as TaskDetails } from "./TaskDetails";
+export { default as Login } from "./Login";
+export { default as Signup } from "./Signup";
